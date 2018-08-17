7d380dbb410d21ca790ec9101dcc81628f89221a
n.sokolov <n.sokolov.sis@gmail.com>
2018-08-17 18:32:21 +0700

    init

---
+++ src/provider/index.js
@@ -0,0 +1,6 @@
+import PropertyProvider from './PropertyProvider';
+
+export default {
+  __init__: [ 'propertiesProvider' ],
+  propertiesProvider: [ 'type', PropertyProvider ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/index.js'
+    },
+    // resolveLoader: {
+    //     modules: [ 'src/provider' ],
+    //     mainFields: [ 'app' ]
+    // },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Managements',
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    'html-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.(xml)$/,
+                use: [
+                    'xml-loader'
+                ]
+            },
+            {
+                test: /\.(bpmn)$/,
+                use: [
+                    'raw-loader'
+                ]
+            }
+        ]
+    },
+};
